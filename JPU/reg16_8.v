67899c65e05baf063944153285d56a6f962420d0
jph83405 <jph83405@uga.edu>
2019-06-17 16:54:25 -0400

    You can now access two registers (one is useless oops)

--- JPU/reg16_8.v
+++ JPU/reg16_8.v
@@ -2,10 +2,13 @@ module reg16_8
 (
   input wire clock,
   input wire enable_write,
+  input enable,
   input wire [15:0] data_in,
   input wire enable_read,
   output reg [15:0] O_dataA,
-  input wire [2:0] rd_wr_addr
+  output reg [15:0] O_dataB,
+  input wire [2:0] rd_wr_addrA,
+  input wire [2:0] rd_wr_addrB
   );
 
   reg [15:0] regs [7:0]; //8 16 bit registers. Select which one to read/write to
@@ -13,12 +16,23 @@ module reg16_8
 
 
   always @ ( posedge clock ) begin
-    if(enable_read)
-      O_dataA <= regs[rd_wr_addr];
-    else if (enable_write)
-      regs[rd_wr_addr] <= data_in;
-    else
-      O_dataA <= 16'h0000;
+    if(enable==1)
+    begin
+      if(enable_read)
+      begin
+        O_dataA <= regs[rd_wr_addrA];
+        O_dataB <= regs[rd_wr_addrB];
+      end
+      else if (enable_write)
+        begin
+        regs[rd_wr_addrA] <= data_in; //always write to the rd_wr_addrA address
+        end
+      else
+      begin
+        O_dataA <= 16'h0000;
+        O_dataB <= 16'h0000;
+      end
+      end
     end
 
 endmodule // 8x16 bit register file.
--- JPU/reg16_8_tb.v
+++ JPU/reg16_8_tb.v
@@ -1,9 +1,12 @@
 module reg16_8tb ();
   reg clock;
   wire [15:0] O_dataA;
+  wire [15:0] O_dataB;
+  reg enable;
   reg enable_write;
   reg enable_read;
-  reg [2:0] rd_wr_addr;
+  reg [2:0] rd_wr_addrA;
+  reg [2:0] rd_wr_addrB;
   reg [15:0] data_in;
 
   always begin
@@ -14,19 +17,24 @@ module reg16_8tb ();
     $dumpfile("reg16_8tb.vcd");
     $dumpvars;
     clock=0;
+    enable=1;
     enable_write = 1;
     enable_read = 0;
-    rd_wr_addr=3'b001;
+    rd_wr_addrA=3'b001;
     data_in=16'hff31;
+    #10
+    rd_wr_addrA=3'b011;
+    data_in=16'h6543;
     #10
     enable_write=0;
     enable_read=1;
-    rd_wr_addr=3'b001;
+    rd_wr_addrA=3'b001;
+    rd_wr_addrB=3'b011;
     #10
     $finish;
   end
-  reg16_8 myreg(clock,enable_write,data_in,enable_read,
-    O_dataA,rd_wr_addr);
+  reg16_8 myreg(clock,enable_write,enable,data_in,enable_read,
+    O_dataA,O_dataB,rd_wr_addrA,rd_wr_addrB);
 
 
 
